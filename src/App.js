451ca865730360b8bbbea75899d355515bfcac68
Thea <theacsullivan@gmail.com>
2023-09-05 19:37:30 +0100

    Added name App to function in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-export default function () {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">Dictionary</header>
