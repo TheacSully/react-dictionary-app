e3ed1e8480664543d7e1241240ef12d074f2344c
Thea <theacsullivan@gmail.com>
2023-09-06 18:24:21 +0100

    Created basic search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">Dictionary</header>
+    <div className="container">
+      <div className="App">
+        <header className="App-header">Dictionary</header>
+        <Dictionary />
+        <footer className="text-center"> Coded by Thea Sullivan</footer>
+      </div>
     </div>
   );
 }
