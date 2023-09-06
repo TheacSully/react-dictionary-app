c03a2e486b4f18b07b080fa15a189b90f737a806
Thea <theacsullivan@gmail.com>
2023-09-06 18:48:34 +0100

    integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword}definition`);
+    alert(`searching for ${keyword} definition`);
+    let apiKey = "ct90a4732c5fd752o670f2a66b23ca38";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
