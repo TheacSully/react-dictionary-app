e6173ba874d0b88bef49dea332be9f0efd88be3d
Thea <theacsullivan@gmail.com>
2023-09-06 19:20:38 +0100

    Added initial CSS to App.js & Dictionary.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./App.css";
+import dictionaryHead from "./dictionaryHead.png";
+import theasullivan from "./theasullivan.png";
+import opensource from "./opensource.png";
+import github from "./github.png";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="container">
       <div className="App">
-        <header className="App-header">Dictionary</header>
+        <header className="header">
+          <img src={dictionaryHead} alt="dictionary logo" />
+        </header>
         <Dictionary />
-        <footer className="text-center"> Coded by Thea Sullivan</footer>
+        <footer className="text-center">
+          {" "}
+          Coded by <img src={theasullivan} /> and <img src={opensource} /> on{" "}
+          <a href="https://github.com/TheacSully/react-dictionary-app">
+            <img src={github} />{" "}
+          </a>
+        </footer>
       </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
+import searchlogo from "./searchlogo.png";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
@@ -11,7 +12,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`searching for ${keyword} definition`);
+    alert(`searching for definition of "${keyword}"`);
     let apiKey = "ct90a4732c5fd752o670f2a66b23ca38";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -24,7 +25,9 @@ export default function Dictionary() {
     <div className="Dictionary">
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
-        <button>Search</button>
+        <button>
+          <img src={searchlogo} alt="search logo" />
+        </button>
       </form>
     </div>
   );
