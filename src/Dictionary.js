be3d26cc4409cfe683d7051a54184249d43d804c
Thea <theacsullivan@gmail.com>
2023-09-09 10:58:09 +0100

    Created Results component & Meanings component but come up against a brick wall with an issue with my .map array

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 import axios from "axios";
 import searchlogo from "./searchlogo.png";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response);
+    //console.log(response.data.meanings[0].definition);
+    setResults(response.data.meanings[0].definition);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`searching for definition of "${keyword}"`);
+    //alert(`searching for definition of "${keyword}"`);
     let apiKey = "ct90a4732c5fd752o670f2a66b23ca38";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
@@ -29,6 +32,7 @@ export default function Dictionary() {
           <img src={searchlogo} alt="search logo" />
         </button>
       </form>
+      <Results results={results} />
     </div>
   );
 }
