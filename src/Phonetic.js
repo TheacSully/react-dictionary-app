d3ada1f7c2c761998df6338c83f26137c651391e
Thea <theacsullivan@gmail.com>
2023-09-17 16:56:40 +0100

    removed speaker logo when no audio is present

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,12 +3,16 @@ import speaker from "./speaker.png";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        <img src={speaker} alt="speaker icon" />
-      </a>
-      {props.phonetic.text}
-    </div>
-  );
+  if (props.phonetic.audio) {
+    return (
+      <div className="Phonetic">
+        {props.phonetic.text}
+        <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+          <img src={speaker} alt="speaker icon" />
+        </a>
+      </div>
+    );
+  } else {
+    return <div className="PhoneticText">{props.phonetic.text}</div>;
+  }
 }
